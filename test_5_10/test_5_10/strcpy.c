e8ee38d0182d3ca9a229644bccb44ec3a5d177c3
Huangxiaobaia <73419092+Huangxiaobaia@users.noreply.github.com>
2021-05-11 20:05:17 +0800

    提交代码

---
+++ test_5_10/test_5_10/strcpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <assert.h>
+//模拟实现库函数：strcpy
+char* my_strcpy(char* dst, const char* src) {
+	char* cp = dst;
+	assert(dst && src);
+	while (*dst++ = *src++)
+		;
+	return cp;
+}
+void test1() {
+	char arr1[] = "I love  you      ";
+	char arr2[] = "I not love you ";
+	printf("%s\n", my_strcpy(arr1, arr2));
+
+}
+int main() {
+	test1();
+	system("pause");
+	return 0;
+}
