8ed85a261f698a1c9910e4055cf64b4b8db44bc7
Huangxiaobaia <73419092+Huangxiaobaia@users.noreply.github.com>
2021-07-04 21:44:59 +0800

    7.4

--- C++Primer/第12章/ch12.cpp
+++ C++Primer/第12章/ch12.cpp
@@ -1,8 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include "strblob.h"
 #include <iostream>
 #include <vector>
 #include <memory>
+#include "StrBlobPtr.h"
+#include <fstream>
 using namespace std;
 //Á·Ï°12.2
 void test01() {
@@ -63,6 +64,95 @@ void test03()
 	unique_ptr<string>p1(new string("abc"));
 
 }
+//Á·Ï°12.20
+void test04()
+{
+	ifstream ifs("µ¥´Ê.txt");
+	StrBlob blob;
+	for (string str; getline(ifs, str);)
+		blob.push_back(str);
+	for (ConstStrBlobPtr pbeg(blob.begin()), pend(blob.end()); pbeg != pend; pbeg.incr())
+		cout << pbeg.deref() << endl;
+}
+//Á·Ï°12.23
+void test05()
+{
+	char* concatenate_string = new char[strlen("hello""world") + 1]();
+	strcat(concatenate_string, "hello");
+	strcat(concatenate_string, "world");
+	cout << concatenate_string << endl;
+	delete[]concatenate_string;
+
+	string str1{ "hello" }, str2{ "world" };
+	cout << str1 + str2 << endl;;
+}
+//Á·Ï°12.24
+void test06()
+{
+	int size{ 0 };
+	cin >> size;
+	char* input = new char[size + 1]();
+	cin.ignore();
+	cout << "input the string: ";
+	cin.get(input,size+1);
+	cout << endl<<input;
+	delete[] input;
+}
+//Á·Ï°12.26
+void test07()
+{
+	int n = 5;
+	allocator<string>alloc;
+	auto const p = alloc.allocate(n);
+	string s;
+	auto q = p;
+	while (q != p + n && cin >> s)
+		alloc.construct(q++, s);
+	while (q != p)
+	{
+		cout << *--q << " ";
+		alloc.destroy(q);
+	}
+	alloc.deallocate(p, n);
+}
+//Á·Ï°12.27
+#include <map>
+#include <set>
+#include <sstream>
+#include <algorithm>
+void test08()
+{
+	ifstream file("µ¥´Ê.txt");
+	vector<string>input;
+	map<string, set<decltype(input.size())>>dictionary;
+	decltype(input.size()) line_no{ 0 };
+	for (string line; getline(file, line); ++line_no)
+	{
+		input.push_back(line);
+		istringstream line_stream(line);
+		for (string text, word; line_stream >> text; word.clear())
+		{
+			remove_copy_if(text.begin(), text.end(), back_inserter(word), ispunct);
+			dictionary[word].insert(line_no);
+		}
+	}
+	while (true)
+	{
+		cout << "enter word to look for,or q to quit: ";
+		string s;
+		if (!(cin >> s) || s == "q")break;
+		auto found = dictionary.find(s);
+		if (found != dictionary.end())
+		{
+			cout << s << " occurs " << found->second.size() << (found->second.size() > 1 ? "times" : "time") << endl;
+			for (auto i : found->second)
+				cout << "\t(line" << i + 1 << ")" << input.at(i) << endl;
+
+		}
+		else
+			cout << s << " occurs 0 time" <<endl;
+	}
+}
 int main()
 {
 	//test01();
@@ -70,5 +160,10 @@ int main()
 	//test02_3(vec) << endl;
 	//destination dest("202.118.176.67", 3316);
 	//f(dest);
+	//test04();
+	//test05();
+	//test06();
+	//test07();
+	test08();
 	return 0;
 }
